c6fccb33410a598acafe979e4b612b5803dff144
Abel Castillo <abel.castillo101@gmail.com>
2019-04-17 21:40:56 -0700

    code refactoring

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # List Pagination and Filtering
+
+A list of students information is displayed in a single page.
+
+Only using vanilla JavaScript to create pagination to enhance the usability of the web page.
+
+For this project the only allowed file to modify was the js/script.js file, no changes where allowed in the HTML file.
+
+In additon an input search bar was created to generate the search results with pagination included. If no user found then an error will be displayed in place of the student list.
--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
+//Grade Goal: "Exceed Expectations"
+
 //Create an array of all the students list items
 let students = document.querySelectorAll("li");
-//Numeber of tiems to be displayed per page
+//Number of items to be displayed per page
 const itemsPerPage = 10;
+
+//taarget main <div class='page'></div>
+const page = document.querySelector(".page");
 
 //Create Search bar for students and append to '.page-header'
 const pageHeader = document.querySelector(".page-header");
@@ -17,20 +22,25 @@ studentSearch.innerHTML = `
       <button>Search</button>`;
 pageHeader.appendChild(studentSearch);
 
-//Search functionality
-const search = e => {
+//Search bar functionality
+const searchInput = document.querySelector(".search-input");
+//Search list filter in real time with 'keyup'
+searchInput.addEventListener("keyup", e => {
   //Remove the '<h3>' result not found error
   const err = document.querySelector(".error");
   if (err) {
     err.remove();
   }
-  //fnd the matching name or email in students array and push to filterStudents array
+  //fnd the matching name in students array and push to filterStudents array
   let filterStudents = [];
   students.forEach(student => {
+    //  convert students name to lower case and the input to lower case, as well as use trim() to ignore whitespace before or after the input search.
     if (
-      student.textContent
-        .toLocaleLowerCase()
-        .includes(e.target.value.toLowerCase())
+      student
+        .querySelector("h3")
+        .textContent.toLocaleLowerCase()
+        .trim()
+        .includes(e.target.value.toLowerCase().trim())
     ) {
       filterStudents.push(student);
       student.style.display = "";
@@ -44,25 +54,14 @@ const search = e => {
 
   //if no search results found create <h3> with error message
   if (filterStudents.length <= 0) {
-    const page = document.querySelector(".page");
     const h3 = document.createElement("h3");
-    h3.textContent = `Sorry, no students named "${e.target.value}".`;
-    h3.style.color = "red";
-    h3.className = "error";
     page.appendChild(h3);
+    h3.outerHTML = `<h3 class='error' style='color: red;'>Sorry, no students named "${
+      e.target.value
+    }".</h3>`;
   }
   //recreate new student list and pagination button
   appendPageLinks(filterStudents);
-};
-
-const searchInput = document.querySelector(".search-input");
-//Search list filter in real time
-searchInput.addEventListener("keyup", e => {
-  search(e);
-});
-//search button if keyup event does not trigger
-searchInput.addEventListener("change", e => {
-  search(e);
 });
 
 //Show only 10 items per page, hide the rest of the items.
@@ -80,38 +79,41 @@ const showPage = (list, page) => {
 
 //pass the studets list to display search results and break list into pagination links
 const appendPageLinks = list => {
-  //append div with class name '.pagination'  to '.page'
-  const page = document.querySelector(".page");
-  const pagination = document.createElement("div");
-  pagination.className = "pagination";
-  page.appendChild(pagination);
-  pagination.innerHTML = `<ul class='pagination-list'></ul>`;
   //divide the length of student results by 10
   const pages = Math.ceil(list.length / 10);
-  //form a list of the pagination list items
+  //Create list items for amount of pages
   let paginationListItems = "";
   for (let i = 1; i <= pages; i++) {
     paginationListItems += `
     <li class='pagination-list-item'>
       <a href='#'>${[i]}</a>
-    <li>`;
+    </li>`;
   }
-  //append pagination list items to <ul>
-  const paginationList = document.querySelector(".pagination-list");
-  paginationList.innerHTML = paginationListItems;
+  //create <div> with pagination <ul>
+  const pagination = document.createElement("div");
+  page.appendChild(pagination);
+  pagination.outerHTML = `
+   <div class='pagination'>
+      <ul class='pagination-list'>
+         ${paginationListItems}
+      </ul>
+   </div>`;
 
-  //put all pagination <li>'s into an array
-  const pageList = document.querySelectorAll(".pagination-list-item");
+  //put all pagination <a>'s into an array
+  const pageList = document.querySelectorAll(".pagination-list-item a");
   if (pageList.length > 0) {
-    pageList[0].firstElementChild.className = "active";
+    pageList[0].className = "active";
   }
   //By deafult show items with page 1
   showPage(list, 1);
-  //when clicked on page remove link class '.active' from all links and apply to target item only anf pass the page number to showPage()
-  paginationList.addEventListener("click", e => {
-    pageList.forEach(el => el.firstElementChild.removeAttribute("class"));
-    e.target.className = "active";
-    showPage(list, e.target.textContent);
+  //when clicked on page number remove link class '.active' from all links and apply to target item only anf pass the page number to showPage()
+  const paginationPageLinks = document.querySelectorAll(".pagination a");
+  paginationPageLinks.forEach(page => {
+    page.addEventListener("click", e => {
+      paginationPageLinks.forEach(el => el.removeAttribute("class"));
+      e.target.className = "active";
+      showPage(list, e.target.textContent);
+    });
   });
 };
 
